026fd2f748f193f0c5a847529b5810e86a7cebdb
邓江洲 <707703386@qq.com>
2020-01-13 13:57:59 +0800

    first commit

---
+++ SwiftJavaScriptCore/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  SwiftJavaScriptCore
+//
+//  Created by myl on 16/6/8.
+//  Copyright © 2016年 Mayanlong. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+
+
+}
+
